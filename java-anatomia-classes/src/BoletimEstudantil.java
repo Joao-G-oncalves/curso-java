9ac60a2db7e9127d69d2225cb0e99bb35cb6d407
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-06-09 18:58:46 -0300

    update anatomia-das-classes

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,12 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        
+        if(mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if(mediaFinal == 6)
+            System.out.println("Prova MINERVA");
+        else
+            System.out.println("APROVADO!");
+    }
+}
