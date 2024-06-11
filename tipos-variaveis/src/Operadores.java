861f5cc1599ccbebbf063c4e2d0b6ddbe8a1b2a0
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-06-11 18:41:42 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,28 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // concatenação
+        // String nomeCompleto = "LINGUAGEM" + "JAVA";
+        // System.out.println(nomeCompleto);
+
+        int numero = 5;
+
+        // System.out.println(-numero);
+
+        // numero = - numero;
+
+        // System.out.println(numero);
+        
+        // numero = - numero; transforma negativo em positivo, bem como, na matemática
+
+        // numero = numero * -1; // Outra maneira de transformar negativo em positivo.
+        
+        numero += 7; // incrementar uma variável criada
+
+        numero++; // incrementar 1 
+
+        System.out.println(numero);
+
+
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,13 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
+        String nome = "João Gonçalves";
         short numeroCurto = 1;
         int numeroNormal = 3500;
         float pi = 3.14f; // float termina com F
         long numeroLongo = 34168416846L; // long termina com L
 
         final double VALOR_DE_PI = 3.14; // constante
-
-
     }
 }
