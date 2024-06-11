dec98721e50ceb5189404a2c730833e6fa43629b
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-06-11 19:10:18 -0300

    update operadores

--- tipos-variaveis/src/OperadoresPart2.java
+++ tipos-variaveis/src/OperadoresPart2.java
@@ -1,17 +1,26 @@
 public class OperadoresPart2 {
     public static void main(String[] args) {
-        String nomeUm = "João";
-        String nomeDois = "Gonçalves";
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+        //uma expressão relacional podem representar valor booleano : 4 < 7 "true";
+
+        if (condicao1 && condicao2){
+            System.out.println("As duas condições sao verdadeiras!");
+        }
+        if (condicao1 || condicao2){
+            System.out.println("Uma das duas condições é verdadeira!");
+        }
+
+        System.out.println("Fim!");
         
-        System.out.println(nomeUm.equals(nomeDois));
+        
+        // String nomeUm = "João";
+        // String nomeDois = "Gonçalves";
+        
+        // System.out.println(nomeUm.equals(nomeDois));
 
         //System.out.println(nomeUm == nomeDois);
-        
-        
-        
-        
-        
-        
+                
         
         
         // int numero1, numero2;
