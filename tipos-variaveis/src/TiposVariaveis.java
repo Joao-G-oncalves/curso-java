62fc8091d8f329250c457148ea5ad88ac7c936cc
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-06-11 19:13:15 -0300

    Update TiposVariaveis.java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,12 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2500;
-        String nome = "João Gonçalves";
-        short numeroCurto = 1;
-        int numeroNormal = 3500;
-        float pi = 3.14f; // float termina com F
+        double salarioMinimo = 2500; // numero real mais usado
+        String nome = "João Gonçalves"; // palavras
+        short numeroCurto = 1; // numero curto que usam menos espaço na memória
+        int numeroNormal = 3500; // numero inteiro mais usado
+        float pi = 3.14f; // float termina com F 
         long numeroLongo = 34168416846L; // long termina com L
 
-        final double VALOR_DE_PI = 3.14; // constante
+        final double VALOR_DE_PI = 3.14; // constante precisa ter a palavra reservada "final"
     }
 }
