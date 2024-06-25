4d4407c66ab13b2d7d7304840acd10b2d4c65143
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-06-25 20:37:00 -0300

    update switch case

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,22 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "x";
+        switch (sigla) {
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M":{
+                System.out.println("MEDIO");
+                break;
+            }
+            case "G":{
+                System.out.println("GRANDE");
+                break;
+            }
+            default:{
+                System.out.println("INDEFINIDO");
+            }
+        }
+    }
+}
