463d64cb0d0d683f547fa951e1779ad804b80892
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-06-25 20:28:10 -0300

    exemplo utilizando operador ternário

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -8,13 +8,17 @@ public class ResultadoEscolar {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         System.out.println("Digite sua nota: ");
         int nota = scanner.nextInt();
-        if (nota >= 7){
-            System.out.println("Aprovado!");
-        }
-        else if(nota >= 5 && nota <7) {
-            System.out.println("Prova de Recuperação!");
-        }else{
-            System.out.println("Reprovado!");
-        }
+        // if (nota >= 7){
+        //     System.out.println("Aprovado!");
+        // }
+        // else if(nota >= 5 && nota <7) {
+        //     System.out.println("Prova de Recuperação!");
+        // }else{
+        //     System.out.println("Reprovado!");
+        // }
+        //OPERADOR TERNÁRIO -->
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
     }
 }
