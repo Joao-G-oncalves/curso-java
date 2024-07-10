9dbe98348cd5a8cba19dfd307056ca35e788a09e
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-07-10 19:58:45 -0300

    update exemplo while

---
+++ controle-fluxo/src/ExemploWhile.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.ThreadLocalRandom;
+public class ExemploWhile {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+        while (mesada > 0) {
+            double valorDoce = valorAleatorio();
+            if (valorDoce > mesada){
+                valorDoce = mesada;
+            }
+            System.out.println("Doce do valor " + valorDoce + "Adicionado no carrinho");
+            mesada -= valorDoce;
+        }
+        System.out.println("Mesada: " + mesada);
+        System.out.println("Joaozinho gastou toda a sua mesada em doces!");
+    }
+    public static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+
+}
