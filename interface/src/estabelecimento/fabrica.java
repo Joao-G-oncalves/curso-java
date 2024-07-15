d2677b114256d82074ee02d9b040afddff2d9f1f
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-07-15 19:59:56 -0300

    last commit to interface

--- interface/src/estabelecimento/fabrica.java
+++ interface/src/estabelecimento/fabrica.java
@@ -11,11 +11,9 @@ public class fabrica {
     public static void main(String[] args) {
     
         EquipamentoMultifuncional em = new EquipamentoMultifuncional();
-        
         Impressora impressora = em;
         Digitalizadora digitalizadora = em;
         Copiadora copiadora = em;
-        
 
         impressora.imprimir();
         digitalizadora.digitalizar();
