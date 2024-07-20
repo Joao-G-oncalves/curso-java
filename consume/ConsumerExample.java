df739088501ef0d87fddd39a5b819c3b714cd917
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-07-20 18:46:32 -0300

    update ComsumerExample

--- consume/ConsumerExample.java
+++ consume/ConsumerExample.java
@@ -2,7 +2,6 @@ package consume;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class ConsumerExample {
     public static void main(String[] args) {
