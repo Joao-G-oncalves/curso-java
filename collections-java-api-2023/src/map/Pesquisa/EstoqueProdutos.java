fbbd23f12bf6ea4eccf14236943c9984a4601a92
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-07-20 14:56:40 -0300

    update Map.pesquisa

---
+++ collections-java-api-2023/src/map/Pesquisa/EstoqueProdutos.java
@@ -0,0 +1,44 @@
+package map.Pesquisa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EstoqueProdutos {
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco){
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProdutos(){
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calcularValorTotalEstoque(){
+        double valorTotalEstoque = 0d;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for(Produto p: estoqueProdutosMap.values()){
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if (!estoqueProdutosMap.isEmpty()){
+            for(Produto p: estoqueProdutosMap.values()){
+                if(p.getPreco() > maiorPreco){
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
+    
+}
