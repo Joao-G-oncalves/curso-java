b4d348a842b52ff01685ac8a477e61d3470a9d99
João Gonçalves da Silva Júnior <zaquir01@gmail.com>
2024-07-20 17:22:45 -0300

    update ConsumerExample

---
+++ consume/ConsumerExample.java
@@ -0,0 +1,25 @@
+package consume;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        // numeros.stream().forEach(new Consumer<Integer>() {
+
+        //     @Override
+        //     public void accept(Integer n) {
+        //         if (n % 2 == 0) {
+        //             System.out.println("numero " + n);
+        //         }
+        //     }
+
+        // });
+
+        numeros.forEach(n -> {if(n % 2 ==0){System.out.println(n);}});
+
+    }
+}
